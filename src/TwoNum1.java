a75b78dbf3bcb1416003809cc9ba3299ff4259e1
vivian <lyh5190609@gmail.com>
2023-09-15 15:18:13 -0400

    Leetcode

--- src/TwoNum1.java
+++ src/TwoNum1.java
@@ -1,11 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Yihan
  * @date 9/15/2023 - 2:42 PM
  */
 public class TwoNum1 {
-    public static void main(String[] args) {
-        System.out.println("mtmb are best girls");
-        System.out.println("null");
-
+    public int[] twoSum(int[] nums, int target) {
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return null;
+        Map<Integer,Integer> numMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target-nums[i];
+            if(numMap.containsKey(complement)){
+                return new int[]{numMap.get(complement),i};
+            }
+            numMap.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
