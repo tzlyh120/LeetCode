77912c56920e6955c79c05dc86c8363adbf76507
tzlyh120 <40650562+tzlyh120@users.noreply.github.com>
2023-09-15 15:15:37 -0400

    Update TwoNum1.java

--- src/TwoNum1.java
+++ src/TwoNum1.java
@@ -3,9 +3,15 @@
  * @date 9/15/2023 - 2:42 PM
  */
 public class TwoNum1 {
-    public static void main(String[] args) {
-        System.out.println("mtmb are best girls");
-        System.out.println("null");
-
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> numMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target-nums[i];
+            if(numMap.containsKey(complement)){
+                return new int[]{numMap.get(complement),i};
+            }
+            numMap.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
